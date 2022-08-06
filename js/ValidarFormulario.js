e337ca380791deb8e317d944e35e145043b52e57
Santiago825 <santiagoorjuela812@gmail.com>
2022-08-06 00:56:26 -0500

    validacion y envio de email

--- js/ValidarFormulario.js
+++ js/ValidarFormulario.js
@@ -1,13 +1,14 @@
 var myform = document.getElementById("formulario");
+var ventanaAceptar=document.getElementById("container_alert_validacion")
 
-function camposVacios(valor,evento){
+let=camposVacios= (valor,evento)=>{
 
     if(valor.value.length==0){
         evento.style.display="flex";
-        return false
+        return 0
     }else{
         evento.style.display="none";
-        return true 
+        return 1 
     }
 }
 function validarFormulario(){
@@ -18,20 +19,41 @@ function validarFormulario(){
     let errorName=document.getElementById("errorName")
     let errorEmail=document.getElementById("errorEmail")
     let errorMessage=document.getElementById("errorMessage")
+    let contador=0;
+   
 
+    
+    
+    contador+=camposVacios(message,errorMessage);
 
-    camposVacios(nombre,errorName);
-    camposVacios(message,errorMessage);
+    contador+=camposVacios(nombre,errorName);
+
     if(!expresion.test(email)){
         errorEmail.style.display="flex";
     }else{
-        errorEmail.style.display="none"; 
+        errorEmail.style.display="none";
+        contador+=1 
     }
+
+    return contador
     
     
     
   }
+ 
 
 document.getElementById("btn-enviar").addEventListener("click", function () {
-  myform.submit();
+    ventanaAceptar.style.display="flex";
+    if(validarFormulario()==3){
+        document.body.classList.add('no-scroll');
+        
+        
+        document.getElementById("btn-aceptar").addEventListener("click",()=>{
+            myform.submit();
+        })
+
+
+        
+    }
+  
 });
